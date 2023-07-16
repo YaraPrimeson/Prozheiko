ef533d931a2627e6f308c1d60692bdedcb4be073
AxiomaPartnersDev <axiomapartnersdev@gmail.com>
2023-07-16 21:45:51 +0300

    commit

--- app/admin/admin-tab/price/price-tab.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PriceTab = () => {
-  return <div></div>;
-};
-
-export default PriceTab;
--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -1,24 +1,15 @@
 "use client";
 import React, { useContext, useLayoutEffect, useState } from "react";
 import BlogList from "@/app/blog/BlogList";
-import { AppContext } from "@/app/context/Context";
 import { Metadata } from "next";
 
 const Page = () => {
-  const { getAllBlog, blog } = useContext(AppContext);
-  const { getAllTags, tags } = useContext(AppContext);
   const [activeTag, setActiveTag] = useState<string>("Показати всі");
 
   const handleActiveTag = (tag: string) => {
     setActiveTag(tag);
   };
-  useLayoutEffect(() => {
-    getAllTags();
-    getAllBlog();
-  }, []);
-  useLayoutEffect(() => {
-    console.log(tags);
-  }, [tags]);
+
   return (
     <div>
       {/*<BlogList*/}
