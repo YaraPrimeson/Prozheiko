b38001c07c1ff592206be30ab5bd300b3c8a3280
AxiomaPartnersDev <axiomapartnersdev@gmail.com>
2023-08-09 13:25:16 +0300

    fix gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import "./styles/globals.scss";
-import React, { Suspense } from "react";
+import React from "react";
 import TheHeader from "@/app/components/header/Header";
 import Footer from "@/app/components/footer/Footer";
 import localFont from "next/font/local";
-import Head from "next/head";
-import Analytics from "@/app/Analytics";
 import AllowCookies from "@/app/components/allow-cookies/allow-cookies";
 
 export const metadata = {
@@ -53,9 +51,23 @@ export default function RootLayout({
           content="стоматологія київ, стоматологічна клініка київ, стоматології києва"
         />
         <meta name="robots" content="index,follow" />
-        <Analytics />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5KMKTR2G');`,
+          }}
+        />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5KMKTR2G"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <TheHeader />
         <main style={{ flex: " 1 1 auto" }}>{children}</main>
         <Footer />
