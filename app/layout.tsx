03d774fcc765d6ed298c6cb52845ded430b8e40c
AxiomaPartnersDev <axiomapartnersdev@gmail.com>
2023-08-10 13:26:25 +0300

    fix next.config.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,15 +50,15 @@ export default function RootLayout({
         />
         <meta name="robots" content="index,follow" />
         <title>Стоматологія в Києві | Prozheiko Dental Studio 🦷</title>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5KMKTR2G');`,
-          }}
-        />
+        {/*<script*/}
+        {/*  dangerouslySetInnerHTML={{*/}
+        {/*    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+        {/*    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+        {/*    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+        {/*    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+        {/*    })(window,document,'script','dataLayer','GTM-5KMKTR2G');`,*/}
+        {/*  }}*/}
+        {/*/>*/}
       </head>
       <body>
         <noscript
