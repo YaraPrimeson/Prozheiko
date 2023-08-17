08fca18f249ab418305eb23d7d8e34e800f61a3d
AxiomaPartnersDev <axiomapartnersdev@gmail.com>
2023-08-17 09:11:02 +0300

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,16 +54,26 @@ export default function RootLayout({
         {/*    })(window,document,'script','dataLayer','GTM-5KMKTR2G');`,*/}
         {/*  }}*/}
         {/*/>*/}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-5KMKTR2G" />
 
-        <Script id="google-tag-manager" strategy="beforeInteractive">
+        <Script id="google-analytics">
           {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5KMKTR2G');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'GTM-5KMKTR2G');
         `}
         </Script>
+        {/*<Script id="google-tag-manager" strategy="afterInteractive">*/}
+        {/*  {`*/}
+        {/*(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':*/}
+        {/*    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],*/}
+        {/*    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=*/}
+        {/*    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);*/}
+        {/*    })(window,document,'script','dataLayer','GTM-5KMKTR2G');*/}
+        {/*`}*/}
+        {/*</Script>*/}
       </head>
       <body>
         <noscript
